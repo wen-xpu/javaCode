10c6d0ff56edf27c71ad69d0e9d499b4bc4c8077
wen-xpu <2495385002@qq.com>
2021-12-26 12:21:13 +0800

    TestFun.javaFAN class

--- wen/src/airFun/TestFun.java
+++ wen/src/airFun/TestFun.java
@@ -1,2 +1,18 @@
-package airFun;public class TestFun {
+package airFun;
+
+public class TestFun {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeed(3);
+        fan1.setColor("yellow");
+        fan1.setRadius(10.0);
+        fan1.setOn(true);
+        Fan fan2 = new Fan();
+        fan2.setSpeed(2);
+        fan2.setColor("blue");
+        fan2.setRadius(5);
+        fan2.setOn(false);
+        System.out.println("该风扇的速度，颜色和半径分别为："  + fan1.toString());
+        System.out.println("该风扇的颜色和半径分别为："  + fan2.toString());
+    }
 }
